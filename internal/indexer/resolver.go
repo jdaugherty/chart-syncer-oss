efb43cc41d9fcd4103b23841943d7fba3dd2241a
Tomas Pizarro Moreno <tomas.pizarro@broadcom.com>
2025-12-10 16:44:15 +0100

    fix url passes to NewRepository
    Signed-off-by: Tomas Pizarro Moreno <tomas.pizarro@broadcom.com>

--- internal/indexer/resolver.go
+++ internal/indexer/resolver.go
@@ -10,7 +10,9 @@ import (
 
 // newRemoteRepository creates a remote.Repository for an OCI registry with basic auth
 func newRemoteRepository(u *url.URL, username, password string, insecure bool) (*remote.Repository, error) {
-	repo, err := remote.NewRepository(u.String())
+	// remote.NewRepository expects a reference in format "host/path", not a full URL
+	ref := u.Host + u.Path
+	repo, err := remote.NewRepository(ref)
 	if err != nil {
 		return nil, err
 	}
